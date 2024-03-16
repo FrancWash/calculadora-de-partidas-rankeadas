0bfde2041612e57cfb5f1d91264af7d692775c3f
FrancWash <fvilela216@gmail.com>
2024-03-16 12:26:07 -0300

    Adicionando o retorno com variavel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 //Função vencer ou perder
-winOrLose(210, 95)
+let esultado = winOrLose (210, 95)
+
 function winOrLose(wins, lose = "Esse herói não sabe oque é perder"){
+    let winningBalance = wins - lose
+    console.log("O saldo de vitória do herói é de: " + winningBalance)
     console.log("Quantidade de vitórias do herói: " + wins)
     console.log("Quantidade de derrotas do herói: " + lose)
+    return winningBalance 
 }
+
